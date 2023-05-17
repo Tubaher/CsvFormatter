b9aa851c589b5f2d2bbea6c0753ab14ff61d13ed
Tubaher <diego.suntaxi@outlook.es>
2023-05-17 00:59:24 -0500

    feat(parsing): single file parsing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,64 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
+const axios = require('axios').default
+const csvParser = require('csv-string')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+axios.defaults.baseURL = 'https://echo-serv.tbxnet.com/v1/secret'
+axios.defaults.headers.common.Authorization = 'Bearer aSuperSecretKey'
+axios.defaults.headers.post['Content-Type'] = 'application/json'
 
-module.exports = router;
+async function getFiles () {
+  try {
+    const response = await axios.get('/files')
+    console.log(response.data)
+    return response.data
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+const checkUndefined = (value) => {
+  if (value.text === undefined) {
+    return null
+  } else if (value.number === undefined) {
+    return null
+  } else if (value.hex === undefined) {
+    return null
+  } else {
+    return value
+  }
+}
+
+const parseLines = (csvData) => {
+  return csvData.reduce((previusLine, currentLine) => {
+    const noErrorsLine = checkUndefined(currentLine)
+    if (noErrorsLine) {
+      return [...previusLine, { text: noErrorsLine.text, number: noErrorsLine.number, hex: noErrorsLine.hex }]
+    } else {
+      return previusLine
+    }
+  }, [])
+}
+
+async function getSingleFile (fileName) {
+  try {
+    const response = await axios.get('/file/' + fileName)
+    const csvData = csvParser.parse(response.data, { output: 'objects' })
+    const processedFile = parseLines(csvData)
+    console.log(processedFile)
+    console.log(typeof processedFile)
+    return processedFile
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+router.get('/files/data', async function (req, res, next) {
+  res.type('application/json')
+  res.status(200).send(
+    await getSingleFile('test2.csv')
+  )
+  console.log('works')
+})
+
+module.exports = router
