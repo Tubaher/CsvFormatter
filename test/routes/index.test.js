3c0b3e33576e6df3b91b4a568efe4236842cfeff
Tubaher <diego.suntaxi@outlook.es>
2023-05-18 04:55:39 -0500

    feat(tests): adding happy path tests

---
+++ test/routes/index.test.js
@@ -0,0 +1,28 @@
+const chaiHttp = require('chai-http')
+const chai = require('chai')
+const server = require('../../bin/www')
+const mocha = require('mocha')
+
+chai.should()
+chai.use(chaiHttp)
+
+mocha.describe('CSV Formatter API', () => {
+  mocha.describe('GET /files/data', () => {
+    mocha.it('should return an array of files', (done) => {
+      chai.request(server)
+        .get('/files/data')
+        .end((_, res) => {
+          res.should.have.status(200)
+          done()
+        })
+    })
+    mocha.it('should return an array of files error test', (done) => {
+      chai.request(server)
+        .get('/files/data/error')
+        .end((_, res) => {
+          res.should.have.status(200)
+          done()
+        })
+    })
+  })
+})
